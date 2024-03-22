e9bb9d1f66cd8a24c2f4e54b4f3bf4aa96255f79
MooshPaw <125221816+MooshPaw@users.noreply.github.com>
2024-03-22 14:01:01 -0500

    Update iFacialMocapTrackingInterface.cs

--- VRC_iFacialMocap/src/iFacialMocapTrackingInterface.cs
+++ VRC_iFacialMocap/src/iFacialMocapTrackingInterface.cs
@@ -1,186 +1,177 @@
-using System.Globalization;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using Microsoft.Extensions.Logging;
-using Microsoft.VisualBasic;
+using iFacialMocapTrackingModule;
 using VRCFaceTracking;
+using VRCFaceTracking.Core.Library;
+using VRCFaceTracking.Core.Params.Expressions;
 
-
-namespace iFacialMocapTrackingModule
+public class iFacialMocapTrackingInterface : ExtTrackingModule
 {
-    class iFacialMocapServer
+    iFacialMocapServer server = new();
+    // What your interface is able to send as tracking data.
+    public override (bool SupportsEye, bool SupportsExpression) Supported => (true, true);
+
+    // This is the first function ran by VRCFaceTracking. Make sure to completely initialize 
+    // your tracking interface or the data to be accepted by VRCFaceTracking here. This will let 
+    // VRCFaceTracking know what data is available to be sent from your tracking interface at initialization.
+    public override (bool eyeSuccess, bool expressionSuccess) Initialize(bool eyeAvailable, bool expressionAvailable)
     {
-        static private int _port = 49983; //port
-        private FacialMocapData _trackedData = new();
-        private UdpClient? _udpListener, _udpClient;
-        public FacialMocapData FaceData { get { return _trackedData; } }
+        var state = (eyeAvailable, expressionAvailable);
+        ModuleInformation.Name = "iFacialMocap";
 
-        /// <summary>
-        /// Stops and disposes the clients
-        public void Stop()
+        // Example of an embedded image stream being referenced as a stream
+        var stream = GetType().Assembly.GetManifestResourceStream("VRCFT_iFacialMocap.res.logo.png");
+
+
+        // Setting the stream to be referenced by VRCFaceTracking.
+        ModuleInformation.StaticImages =
+            stream != null ? new List<Stream> { stream } : ModuleInformation.StaticImages;
+
+        //... Initializing module. Modify state tuple as needed (or use bool contexts to determine what should be initialized).
+        server.Connect(ref Logger);
+        return state;
+    }
+
+    // Polls data from the tracking interface.
+    // VRCFaceTracking will run this function in a separate thread;
+    public override void Update()
+    {
+        // Get latest tracking data from interface and transform to VRCFaceTracking data.
+        server.ReadData(ref Logger);
+        if (Status == ModuleState.Active) // Module Status validation
         {
-            if (_udpClient != null) { _udpClient.Close(); _udpClient.Dispose(); }
-            if (_udpListener != null) { _udpListener.Close(); _udpListener.Dispose(); }
-            FaceData.blends.Clear();
-        }
-        /// </summary>
-        public static string GetLocalIPAddress()
-        {
-            var host = Dns.GetHostEntry(Dns.GetHostName());
-            foreach (var ip in host.AddressList)
-            {
-                if (ip.AddressFamily == AddressFamily.InterNetwork)
-                {
-                    return ip.ToString();
-                }
-            }
-            throw new Exception("No connection found!");
-        }
-
-        /// <summary>
-        /// Connects to the Facial Mockup server socket.
-        /// </summary>
-        /// <param name="ipaddress"></param>
-        public void Connect(ref ILogger logger, string ipaddress = "255.255.255.255")
-        {
-            _udpListener = new(_port);
-            _udpClient = new();
-
-            var timeToWait = TimeSpan.FromSeconds(120);
-
-            logger.LogInformation($"Searching iFacialMocap data for {timeToWait.TotalSeconds} seconds on {GetLocalIPAddress()}:{_port}");
-            var asyncResult = _udpListener.BeginReceive(null, null);
-            asyncResult.AsyncWaitHandle.WaitOne(timeToWait);
-            if (asyncResult.IsCompleted)
-            {
-                try
-                {
-
-                    IPEndPoint dstAddr = new(IPAddress.Parse(ipaddress), _port);
-                    string data = "iFacialMocap_sahuasouryya9218sauhuiayeta91555dy3719|sendDataVersion=v2";
-                    byte[] bytes = Encoding.UTF8.GetBytes(data);
-                    _udpClient.Send(bytes, bytes.Length, dstAddr);
-
-                    _udpListener.Client.ReceiveTimeout = 1000;
-                    logger.LogInformation($"Connecting to {GetLocalIPAddress()}:{_port}");
-
-                }
-                catch (Exception e)
-                {
-                    Stop();
-                }
-
-            }
-
-            else
-            {
-                // Could not connect in time, close module
-                logger.LogWarning("Did not receive iFacialMocap data within initialization period, re-initialize the module to try again...");
-                return;
-            }
+            // ... Execute update cycle.
+            //UnifiedTracking.Data.Eye.Left.Openness = ExampleTracker.LeftEye.Openness;
+            //UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.JawOpen] = ExampleTracker.Mouth.JawOpen;
+            UpdateData();
         }
 
+        // Add a delay or halt for the next update cycle for performance. eg: 
+        Thread.Sleep(1);
+    }
 
-        /// <summary>
-        /// Reads and parses the data recieved by the UDP Client, 
-        /// storing the facial data result in the Face Data attributes.
-        /// </summary>
+    // Called when the module is unloaded or VRCFaceTracking itself tears down.
+    public override void Teardown()
+    {
+        //... Deinitialize tracking interface; dispose any data created with the module.
+        server.Stop();
+    }
 
-        public void ReadData(ref ILogger logger)
-        {
-            if (_udpListener != null)
-            {
-                IPEndPoint? RemoteIpEndPoint = null;
-                byte[] receiveBytes = _udpListener.Receive(ref RemoteIpEndPoint);
-                string returnData = Encoding.ASCII.GetString(receiveBytes);
-                string[] blendData = returnData.Split('|')[1..^1];
-                int i = 0;
-                while (i < blendData.Length) //While in the int attributes
-                {
-                    HandleChange(blendData[i], ref logger);
-                    i++;
-                }
+    void UpdateData()
+    {
+        //Could make a dict<UnifiedExpressions,string> or directly assigning Data.Shapes for better performance but can do math here so whatever for now.
+        Console.WriteLine(server.FaceData.BlendValue("jawOpen"));
+        #region Eye Gaze
+        UnifiedTracking.Data.Eye.Left.Gaze.x = server.FaceData.BlendValue("eyeLookOut_L") - server.FaceData.BlendValue("eyeLookIn_L");
+        UnifiedTracking.Data.Eye.Left.Gaze.y = server.FaceData.BlendValue("eyeLookUp_L") - server.FaceData.BlendValue("eyeLookDown_L");
+        UnifiedTracking.Data.Eye.Right.Gaze.x = server.FaceData.BlendValue("eyeLookIn_R") - server.FaceData.BlendValue("eyeLookOut_R");
+        UnifiedTracking.Data.Eye.Right.Gaze.y = server.FaceData.BlendValue("eyeLookUp_R") - server.FaceData.BlendValue("eyeLookDown_R");
+        #endregion
+        #region Eye Openness
+        UnifiedTracking.Data.Eye.Left.Openness = 1.0f - (float)Math.Max(0, Math.Min(1, server.FaceData.BlendValue("eyeBlink_L") +
+                server.FaceData.BlendValue("eyeBlink_L") * server.FaceData.BlendValue("eyeSquint_L")));
 
-            }
-            else
-            {
-                logger.LogError("UDPClient wasn't initialized.");
-            }
-        }
+        UnifiedTracking.Data.Eye.Right.Openness = 1.0f - (float)Math.Max(0, Math.Min(1, server.FaceData.BlendValue("eyeBlink_R") +
+                server.FaceData.BlendValue("eyeBlink_R") * server.FaceData.BlendValue("eyeSquint_R")));
 
-        /// <summary>
-        /// Changes the facial data depending of the assignation recieved.
-        /// </summary>
-        /// <param name="blend"></param>
-        void HandleChange(string blend, ref ILogger logger)
-        {
-            if (blend.Contains('&'))
-            {
+        #endregion
+        #region Eye Blends
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.EyeSquintLeft].Weight = server.FaceData.BlendValue("eyeSquint_L");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.EyeSquintRight].Weight = server.FaceData.BlendValue("eyeSquint_R");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.EyeWideLeft].Weight = server.FaceData.BlendValue("eyeWide_L");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.EyeWideRight].Weight = server.FaceData.BlendValue("eyeWide_R");
+        #endregion
+        #region Pupil
+        //EyeDilation & EyeConstrict default in mid value idk
+        UnifiedTracking.Data.Eye.Left.PupilDiameter_MM = 5f;
+        UnifiedTracking.Data.Eye.Right.PupilDiameter_MM = 5f;
+        UnifiedTracking.Data.Eye._minDilation = 0;
+        UnifiedTracking.Data.Eye._maxDilation = 10;
+        #endregion
+        #region Eye Brow
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.BrowInnerUpLeft].Weight = server.FaceData.BlendValue("browInnerUp_L");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.BrowLowererLeft].Weight = server.FaceData.BlendValue("browDown_L");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.BrowOuterUpLeft].Weight = server.FaceData.BlendValue("browOuterUp_L");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.BrowPinchLeft].Weight = server.FaceData.BlendValue("browDown_L");
 
-                string[] assignVal = blend.Split('&');
-                try
-                {
-                    _trackedData.blends[assignVal[0]] = int.Parse(assignVal[1]);
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.BrowInnerUpRight].Weight = server.FaceData.BlendValue("browInnerUp_R");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.BrowLowererRight].Weight = server.FaceData.BlendValue("browDown_R");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.BrowOuterUpRight].Weight = server.FaceData.BlendValue("browOuterUp_R");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.BrowPinchRight].Weight = server.FaceData.BlendValue("browDown_R");
+        #endregion 
+        #region Nose
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.NoseSneerLeft].Weight = server.FaceData.BlendValue("noseSneer_L");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.NoseSneerRight].Weight = server.FaceData.BlendValue("noseSneer_R");
+        //Default NasalDitalation & NasalConstrict
+        #endregion
+        #region Cheek
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.CheekPuffLeft].Weight = server.FaceData.BlendValue("cheekPuff");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.CheekSquintLeft].Weight = server.FaceData.BlendValue("cheekSquint_L");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.CheekPuffRight].Weight = server.FaceData.BlendValue("cheekPuff");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.CheekSquintRight].Weight = server.FaceData.BlendValue("cheekSquint_R");
+        //No CheekSuck'ing lol
+        #endregion
+        #region Mewing
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.JawLeft].Weight = server.FaceData.BlendValue("jawLeft");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.JawRight].Weight = server.FaceData.BlendValue("jawRight");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.JawOpen].Weight = server.FaceData.BlendValue("jawOpen");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.MouthClosed].Weight = server.FaceData.BlendValue("mouthClose");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.JawForward].Weight = server.FaceData.BlendValue("jawForward");
+        //Default JawBackward, JawClench & JawMandibleRaise
+        #endregion
+        #region Lip 
+        //lips expressions = mouth expressions
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.LipPuckerUpperLeft].Weight = server.FaceData.BlendValue("mouthPucker");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.LipPuckerLowerLeft].Weight = server.FaceData.BlendValue("mouthPucker");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.LipPuckerUpperRight].Weight = server.FaceData.BlendValue("mouthPucker");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.LipPuckerLowerRight].Weight = server.FaceData.BlendValue("mouthPucker");
 
-                }
-                catch (Exception e)
-                {
-                    logger.LogWarning($"Invalid assignation. [{e};;{blend}]");
-                    return;
-                }
-            }
-            else if (blend.Contains('#'))
-            {
-                string[] assignVal = blend.Split('#');
-                try
-                {
-                    string[] unparsedValues = assignVal[1].Split(',');
-                    float[] values = new float[unparsedValues.Length];
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.LipFunnelUpperLeft].Weight = server.FaceData.BlendValue("mouthFunnel");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.LipFunnelLowerLeft].Weight = server.FaceData.BlendValue("mouthFunnel");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.LipFunnelUpperRight].Weight = server.FaceData.BlendValue("mouthFunnel");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.LipFunnelLowerRight].Weight = server.FaceData.BlendValue("mouthFunnel");
 
-                    for (int j = 0; j < unparsedValues.Length; j++)
-                    {
-                        values[j] = float.Parse(unparsedValues[j], CultureInfo.InvariantCulture.NumberFormat);
-                    }
-                    if (assignVal[0] == "=head")
-                    {
-                        if (values.Length == 6)
-                            _trackedData.head = values;
-                        else
-                            logger.LogWarning("Insuficient data to assign head's position");
-                    }
-                    else if (assignVal[0] == "rightEye")
-                    {
-                        if (values.Length == 3)
-                            _trackedData.rightEye = values;
-                        else
-                            logger.LogWarning("Insuficient data to assign right eye's position");
-                    }
-                    else if (assignVal[0] == "leftEye")
-                    {
-                        if (values.Length == 3)
-                            _trackedData.leftEye = values;
-                        else
-                            logger.LogWarning("Insuficient data to assign left eye's position");
-                    }
-                    else
-                    {
-                        logger.LogWarning($"Error on setting {assignVal}.");
-                        return;
-                    }
-                }
-                catch (Exception e)
-                {
-                    logger.LogWarning($"Invalid assignation. [{e};;{blend}]");
-                    return;
-                }
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.LipSuckUpperLeft].Weight = Math.Min(
+            1f - (float)Math.Pow(server.FaceData.BlendValue("mouthUpperUp_L"), 1 / 6f),
+            server.FaceData.BlendValue("mouthRollUpper")
+        );
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.LipSuckLowerLeft].Weight = server.FaceData.BlendValue("mouthRollLower");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.LipSuckUpperRight].Weight = Math.Min(
+            1f - (float)Math.Pow(server.FaceData.BlendValue("mouthUpperUp_L"), 1 / 6f),
+            server.FaceData.BlendValue("mouthRollUpper")
+        );
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.LipSuckLowerRight].Weight = server.FaceData.BlendValue("mouthRollLower");
+        #endregion
+        #region Mouth
+        //not sure if appropiate ussage of shrug
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.MouthRaiserLower].Weight = server.FaceData.BlendValue("mouthShrugLower");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.MouthRaiserUpper].Weight = server.FaceData.BlendValue("mouthShrugUpper");
 
-            }
-            else
-            {
-                logger.LogWarning($"Data cropped.");
-            }
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.MouthUpperLeft].Weight = server.FaceData.BlendValue("mouthLeft");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.MouthLowerLeft].Weight = server.FaceData.BlendValue("mouthLeft");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.MouthUpperUpLeft].Weight = server.FaceData.BlendValue("mouthUpperUp_L");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.MouthLowerDownLeft].Weight = server.FaceData.BlendValue("mouthLowerDown_L");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.MouthCornerPullLeft].Weight = server.FaceData.BlendValue("mouthSmile_L");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.MouthLowerDownLeft].Weight = server.FaceData.BlendValue("mouthLowerDown_L");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.MouthDimpleLeft].Weight = server.FaceData.BlendValue("mouthDimple_L");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.MouthFrownLeft].Weight = server.FaceData.BlendValue("mouthFrown_L");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.MouthPressLeft].Weight = server.FaceData.BlendValue("mouthPress_L");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.MouthStretchLeft].Weight = server.FaceData.BlendValue("mouthStretch_L");
 
-        }
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.MouthUpperRight].Weight = server.FaceData.BlendValue("mouthRight");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.MouthLowerRight].Weight = server.FaceData.BlendValue("mouthRight");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.MouthUpperUpRight].Weight = server.FaceData.BlendValue("mouthUpperUp_R");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.MouthLowerDownRight].Weight = server.FaceData.BlendValue("mouthLowerDown_R");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.MouthCornerPullRight].Weight = server.FaceData.BlendValue("mouthSmile_R");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.MouthLowerDownRight].Weight = server.FaceData.BlendValue("mouthLowerDown_R");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.MouthDimpleRight].Weight = server.FaceData.BlendValue("mouthDimple_R");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.MouthFrownRight].Weight = server.FaceData.BlendValue("mouthFrown_R");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.MouthPressRight].Weight = server.FaceData.BlendValue("mouthPress_R");
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.MouthStretchRight].Weight = server.FaceData.BlendValue("mouthStretch_R");
+
+        //Default MouthUpperDeepenLeft, MouthCornerSlantLeft & MouthTightenerLeft
+        #endregion
+        #region Tongue
+        UnifiedTracking.Data.Shapes[(int)UnifiedExpressions.TongueOut].Weight = server.FaceData.BlendValue("tongueOut");
+        //Not sure if any more
+        #endregion
     }
 }
